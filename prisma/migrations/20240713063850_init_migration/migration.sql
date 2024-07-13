c5ac4c4da5f43955855600b061081e87b7f81e97
oswgg <oswaldo.gg0520@gmail.com>
2024-07-13 00:52:51 -0600

    Initialazing db with prisma

---
+++ prisma/migrations/20240713063850_init_migration/migration.sql
@@ -0,0 +1,31 @@
+-- CreateTable
+CREATE TABLE `Organization` (
+    `uuid` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(100) NOT NULL,
+    `image_url` VARCHAR(191) NULL,
+    `welcome_text` MEDIUMTEXT NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`uuid`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `User` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `organization_uuid` VARCHAR(191) NOT NULL,
+    `username` VARCHAR(50) NOT NULL,
+    `first_name` VARCHAR(50) NOT NULL,
+    `last_name` VARCHAR(50) NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `age` INTEGER NULL,
+    `sex` TINYINT UNSIGNED NULL,
+    `role` ENUM('admin', 'member') NOT NULL,
+    `language` ENUM('es', 'en') NOT NULL DEFAULT 'en',
+    `image_url` VARCHAR(191) NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `User` ADD CONSTRAINT `User_organization_uuid_fkey` FOREIGN KEY (`organization_uuid`) REFERENCES `Organization`(`uuid`) ON DELETE RESTRICT ON UPDATE CASCADE;
